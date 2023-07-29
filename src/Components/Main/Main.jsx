2e864ba8468701b107d72390550df2ca348cdebf
animeshdesh <animeshdesh.aa@gmail.com>
2023-07-29 11:45:12 +0530

    Implimentation final

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -3,6 +3,7 @@ import { ReactComponent as Email } from "../../Assets/Email.svg";
 import { ReactComponent as Gift } from "../../Assets/Gift.svg";
 import { ReactComponent as Arrow01 } from "../../Assets/Arrow01.svg";
 import { ReactComponent as Arrow02 } from "../../Assets/Arrow02.svg";
+import { ReactComponent as Logo } from "../../Assets/Logo.svg";
 import Image01 from "../../Assets/Rectangle01.jpg";
 import Image02 from "../../Assets/Rectangle02.jpg";
 import Image03 from "../../Assets/Rectangle03.jpg";
@@ -12,6 +13,12 @@ import Image06 from "../../Assets/Rectangle06.jpg";
 import person01 from "../../Assets/person01.png";
 import person02 from "../../Assets/person02.png";
 import person03 from "../../Assets/person03.png";
+import Team01 from "../../Assets/Team01.png";
+import Team02 from "../../Assets/Team02.png";
+import Team03 from "../../Assets/Team03.png";
+import Team04 from "../../Assets/Team04.png";
+import Team05 from "../../Assets/Team05.png";
+import Team06 from "../../Assets/Team06.png";
 import Guy from "../../Assets/Guypointing.png";
 import React from "react";
 
@@ -594,7 +601,315 @@ const Main = () => {
               </Grid>
             </Grid>
           </Container>
+          {/* Our Team---------------------------------------------------------> */}
+          <Container sx={{ marginTop: "80px" }}>
+            <Typography
+              variant="h2"
+              sx={{ color: "white", textAlign: "center" }}
+            >
+              Our Team
+            </Typography>
+            <Grid container spacing={2} sx={{ marginTop: "80px" }}>
+              <Grid md={4}>
+                <Box
+                  sx={{
+                    height: "109px",
+                    width: "344px",
+                    display: "flex",
+                    justifyContent: "space-evenly",
+                    alignItems: "center",
+                    gap: "15px",
+                  }}
+                >
+                  <img src={Team01} alt="Team01" />
+                  <Box sx={{ color: "white" }}>
+                    <Typography variant="h5">Danial Def</Typography>
+                    <Typography sx={{ opacity: "30%" }}>301 Cases</Typography>
+                  </Box>
+                </Box>
+              </Grid>
+              <Grid md={4}>
+                <Box
+                  sx={{
+                    height: "109px",
+                    width: "344px",
+                    display: "flex",
+                    justifyContent: "space-evenly",
+                    alignItems: "center",
+                    gap: "15px",
+                    backgroundColor: "#E3B748",
+                    borderRadius: "14px",
+                  }}
+                >
+                  <img src={Team02} alt="Team02" />
+                  <Box sx={{ color: "black" }}>
+                    <Typography variant="h5">Sanfole</Typography>
+                    <Typography sx={{ opacity: "70%" }}>850 Cases</Typography>
+                  </Box>
+                </Box>
+              </Grid>
+              <Grid md={4}>
+                <Box
+                  sx={{
+                    height: "109px",
+                    width: "344px",
+                    display: "flex",
+                    justifyContent: "space-evenly",
+                    alignItems: "center",
+                    gap: "15px",
+                  }}
+                >
+                  <img src={Team03} alt="Team03" />
+                  <Box sx={{ color: "white" }}>
+                    <Typography variant="h5">Colleen</Typography>
+                    <Typography sx={{ opacity: "30%" }}>180 Cases</Typography>
+                  </Box>
+                </Box>
+              </Grid>
+            </Grid>
+            <Grid container spacing={2} sx={{ marginTop: "25px" }}>
+              <Grid md={4}>
+                <Box
+                  sx={{
+                    height: "109px",
+                    width: "344px",
+                    display: "flex",
+                    justifyContent: "space-evenly",
+                    alignItems: "center",
+                    gap: "15px",
+                  }}
+                >
+                  <img src={Team04} alt="Team04" />
+                  <Box sx={{ color: "white" }}>
+                    <Typography variant="h5">Haldone</Typography>
+                    <Typography sx={{ opacity: "30%" }}>212 Cases</Typography>
+                  </Box>
+                </Box>
+              </Grid>
+              <Grid md={4}>
+                <Box
+                  sx={{
+                    height: "109px",
+                    width: "344px",
+                    display: "flex",
+                    justifyContent: "space-evenly",
+                    alignItems: "center",
+                    gap: "15px",
+                  }}
+                >
+                  <img src={Team05} alt="Team05" />
+                  <Box sx={{ color: "white" }}>
+                    <Typography variant="h5">Nik Jeo</Typography>
+                    <Typography sx={{ opacity: "30%" }}>Nik Jeo</Typography>
+                  </Box>
+                </Box>
+              </Grid>
+              <Grid md={4}>
+                <Box
+                  sx={{
+                    height: "109px",
+                    width: "344px",
+                    display: "flex",
+                    justifyContent: "space-evenly",
+                    alignItems: "center",
+                    gap: "15px",
+                  }}
+                >
+                  <img src={Team06} alt="Team06" />
+                  <Box sx={{ color: "white" }}>
+                    <Typography variant="h5">Nik Jeo</Typography>
+                    <Typography sx={{ opacity: "30%" }}>Nik Jeo</Typography>
+                  </Box>
+                </Box>
+              </Grid>
+            </Grid>
+          </Container>
+          {/* FAQ---------------------------------------------------------------> */}
+          <Typography variant="h4" sx={{ color: "white", marginTop: "35px" }}>
+            FAQ
+          </Typography>
+          <Grid
+            container
+            spacing={2}
+            sx={{ marginTop: "50px", paddingBottom: "50px" }}
+          >
+            <Grid md={6}>
+              <Typography
+                sx={{
+                  color: "white",
+                  opacity: "30%",
+                  width: "70%",
+                  marginLeft: "18px",
+                  marginTop: "25px",
+                }}
+              >
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                amet sint.
+              </Typography>
+            </Grid>
+            <Grid md={6}>
+              <Typography variant="h5" sx={{ color: "white" }}>
+                Do I need a personal injury report?
+              </Typography>
+              <Typography
+                sx={{
+                  color: "white",
+                  opacity: "30%",
+                  marginTop: "25px",
+                  marginBottom: "25px",
+                }}
+              >
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                amet sint. Velit officia consequatduis enim velit mollit Exer.
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                amet sint. Velit officia consequatduis enim velit mollit Exer.
+              </Typography>
+              <Box
+                sx={{
+                  borderTop: "2px solid grey",
+                  borderBottom: "2px solid grey",
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+              >
+                <Typography
+                  variant="h5"
+                  sx={{
+                    margin: "40px 0px 40px 0px",
+                    color: "white",
+                  }}
+                >
+                  How much is my case worth?
+                </Typography>
+                <Box
+                  sx={{
+                    width: "28px",
+                    height: "28px",
+                    backgroundColor: "#E3B748",
+                    borderRadius: "50%",
+                    display: "flex",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Typography variant="h5">+</Typography>
+                </Box>
+              </Box>
+              <Box
+                sx={{
+                  borderBottom: "2px solid grey",
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+              >
+                <Typography
+                  variant="h5"
+                  sx={{
+                    margin: "40px 0px 40px 0px",
+                    color: "white",
+                  }}
+                >
+                  What should I do right after car accidect
+                </Typography>
+                <Box
+                  sx={{
+                    width: "28px",
+                    height: "28px",
+                    backgroundColor: "#E3B748",
+                    borderRadius: "50%",
+                    display: "flex",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Typography variant="h5">+</Typography>
+                </Box>
+              </Box>
+              <Box
+                sx={{
+                  borderBottom: "2px solid grey",
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+              >
+                <Typography
+                  variant="h5"
+                  sx={{
+                    margin: "40px 0px 40px 0px",
+                    color: "white",
+                  }}
+                >
+                  How much is my case worth?
+                </Typography>
+                <Box
+                  sx={{
+                    width: "28px",
+                    height: "28px",
+                    backgroundColor: "#E3B748",
+                    borderRadius: "50%",
+                    display: "flex",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Typography variant="h5">+</Typography>
+                </Box>
+              </Box>
+            </Grid>
+          </Grid>
+          <Container sx={{ marginTop: "25px", backgroundColor: "#47474780" }}>
+            <Box sx={{ padding: "30px 30px" }}>
+              <Typography
+                variant="h3"
+                sx={{ textAlign: "center", color: "white" }}
+              >
+                Subscribe Our Newsletter
+              </Typography>
+            </Box>
+          </Container>
         </Container>
+        <Box
+          sx={{
+            backgroundColor: "#111111",
+            height: "10vh",
+            fontFamily: "inter",
+            marginTop: "80px",
+          }}
+        >
+          <Container
+            sx={{
+              padding: "20px 10px",
+              display: "flex",
+              color: "white",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Logo />
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                width: "35%",
+              }}
+            >
+              <Typography>Home</Typography>
+              <Typography>Attorneys</Typography>
+              <Typography>Practice Areas</Typography>
+              <Typography>About Us</Typography>
+            </Box>
+            <Button
+              variant="outlined"
+              sx={{
+                borderColor: "white",
+                color: "white",
+                border: "3px solid white",
+              }}
+            >
+              <Typography>Contact now</Typography>
+            </Button>
+          </Container>
+        </Box>
       </div>
     </>
   );
